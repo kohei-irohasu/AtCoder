3019102227d131258c8c57ac226a1fba8265b727
kohei-irohasu <yktmd059@icloud.com>
2023-12-04 17:48:56 +0900

    commit

---
+++ ABC.323.py
@@ -0,0 +1,9 @@
+# A問題
+S = input()
+if S[1::2] == '0' * 8:
+    print('Yes')
+else:
+    print('No')
+    
+    
+# B問題
--- ABC324.py
+++ ABC324.py
@@ -16,7 +16,34 @@ print('Yes' if N == 1 else 'No')
 
 
 # C問題
+def check(s, t):
+    if len(s) > len(t): 
+        return check(t, s)  # 常に s<t としておく
+    if len(s) < len(t) - 1: # 差が2以上だったらアウト
+        return False
+    i, j, miss = 0, 0, 0
+    while i < len(s):
+        if s[i] == t[j]: # 先頭から1文字ずつ見ていく
+            i += 1
+            j += 1
+        else:
+            miss += 1
+            if miss > 1: # 相違点は2以上だったらアウト
+                return False
+            if len(s) == len(t):
+                i += 1
+            j += 1
+    return True
 
+N, T = input().split()
+N = int(N)
+ans = []
+for i in range(N):
+    S = input()
+    if check(S, T):
+        ans.append(i + 1)
+print(len(ans))
+print(" ".join(map(str, ans))) # 文字列でないと、joinできない
 
 
 # D問題
