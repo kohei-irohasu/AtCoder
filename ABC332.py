422b666e707695425b2b2385f5eec5004ec35532
kohei-irohasu <yktmd059@icloud.com>
2023-12-23 18:22:31 +0900

    commit

--- ABC332.py
+++ ABC332.py
@@ -74,3 +74,43 @@ for i in s:
     if logo > ans:
         ans = logo
 print(ans) 
+
+
+# D問題
+# 数が小さいので幅優先探索。
+# 最小手数 = 最短経路 = BFS
+from collections import deque
+
+h, w = map(int, input().split())
+a = [list(map(int, input().split())) for _ in range(h)]
+b = [list(map(int, input().split())) for _ in range(h)]
+
+# 辞書のキーは不変なオブジェクトである必要があるから
+start_state = tuple(map(tuple, a))
+target_state = tuple(map(tuple, b))
+
+d = {start_state: 0}
+q = deque([a])
+
+while q:
+    a = q.popleft()
+    
+    for i in range(h - 1):
+        b = [row[:] for row in a]
+        b[i], b[i + 1] = b[i + 1], b[i]
+        
+        # すでにその行列をチェックしているかどうか
+        if tuple(map(tuple, b)) not in d:
+            d[tuple(map(tuple, b))] = d[tuple(map(tuple, a))] + 1
+            q.append(b)
+    
+    for j in range(w - 1):
+        b = [row[:] for row in a]
+        for i in range(h):
+            b[i][j], b[i][j + 1] = b[i][j + 1], b[i][j]
+        
+        if tuple(map(tuple, b)) not in d:
+            d[tuple(map(tuple, b))] = d[tuple(map(tuple, a))] + 1
+            q.append(b)
+
+print(d[target_state] if target_state in d else -1)
